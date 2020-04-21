a0024e2ddc33687a5323cb6bf4f3bf108898e420
Muhammad Ali <nerdjfpb@gmail.com>
2020-04-21 22:36:51 +0600

    assign work for everyone

--- src/App.js
+++ src/App.js
@@ -2,7 +2,26 @@ import React from 'react'
 import './App.css'
 
 function App() {
-	return <div className='App'>Hello From Develop Branch</div>
+	return (
+		<div className='App'>
+			<ul>
+				<li>
+					First git checkout -b 'your branchname' ---- ex: feature/loginform
+				</li>
+				<li>All the work</li>
+				<li>Add files using git add .</li>
+				<li>
+					commit using git commit -m "details about the work" -- ex: signup
+					button and input added
+				</li>
+				<li>
+					push the git - git push -u origin 'your branchname' [for the first
+					time when creating the branch]
+				</li>
+				<li>push the git - git push [after every changes]</li>
+			</ul>
+		</div>
+	)
 }
 
 export default App
