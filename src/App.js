f1d73c0085b9622e07e0ac55e6b8d18e87ccff22
Muhammad Ali <nerdjfpb@gmail.com>
2020-04-21 22:28:57 +0600

    Develop branch first commit

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react'
 import './App.css'
 
 function App() {
-	return <div className='App'>Hello World</div>
+	return <div className='App'>Hello From Develop Branch</div>
 }
 
 export default App
